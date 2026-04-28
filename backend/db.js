af3d9a928cd1cd9457e4ce9ff564f715c6e127a2
samarthf-devops <samarthf@valueaddsofttech.com>
2026-04-28 17:34:02 +0530

    Initial commit of Weather app using docker and github actions

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/weatherApp");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/weatherApp");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.error("MongoDB Error ❌", error);
